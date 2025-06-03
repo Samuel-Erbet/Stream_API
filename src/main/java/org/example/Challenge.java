4cbf36816b0d07c2e5aae63cf2768bd33f36211f
Samuel Erbet <samuelerbet@gmail.com>
2025-06-03 19:30:09 -0300

    modificação no challenge

--- src/main/java/org/example/Challenge.java
+++ src/main/java/org/example/Challenge.java
@@ -1,4 +1,22 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 public class Challenge {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        List<Integer> ordenada = numeros.stream()
+                .sorted()
+                .toList();
+
+        System.out.println("ordenada\n" + ordenada);
+
+
+
+    }
 }
